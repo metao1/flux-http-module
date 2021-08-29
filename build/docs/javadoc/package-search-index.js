029862cdb0421dcd68dfeb90dfa4c7ae579ffa64
Mehrdad Karami <mehrdad.karami@ippen-digital.de>
2021-08-29 13:32:21 +0200

    updated LICENSE

---
+++ build/docs/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.metao.http.exception"},{"l":"com.metao.http.model.http"},{"l":"com.metao.http.model.request"},{"l":"com.metao.http.model.response"},{"l":"com.metao.http.property"},{"l":"com.metao.http.service"},{"l":"com.metao.http.utils"}];updateSearchResults();
